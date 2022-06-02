a929068ed3d8ad8da27f139ea7056864fc0e3df9
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-06-02 11:34:00 +0600

    initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+require("express-async-errors");
+
+//extra security packages
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const bodyParser = require("body-parser");
+const rateLimiter = require('express-rate-limit')
+
+//db connection string
+const connectDB = require('./db/connect')
+
+const express = require("express");
+
+
+const app = express();
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(xss());
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listeniing on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
