644cead63888605b0d0db7f7581dbf9de8b16051
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-06-19 17:18:23 +0600

    post model

---
+++ backend/models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please provide a title"],
+    maxlength: 50,
+  },
+  message: {
+    type: String,
+    required: [true, "Please provide a message"],
+  },
+  creator: {
+    type: String,
+    required: [true, "Please provide a creator"],
+  },
+//   createdBy: {
+//     type: mongoose.Types.ObjectId,
+//     ref: "User",
+//     required: [true, "Please provide a user"],
+//   },
+  tags: {
+    type: String,
+    require: [true, "Please provide atleast one tag"]
+  },
+  file: {
+    type: String,
+  },
+  likeCount: {
+    type: Number,
+    default: 0
+  }
+}, {timestamps: true});
+
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
