6d3677a4affe38733f7564035f3ebc8d6b562579
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-06-20 15:20:22 +0600

    GetallPosts method

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,9 +1,18 @@
-
+const Post = require('../models/Post')
 
 
 const getAllPosts = async (req, res) => {
-    res.send('All posts fetched')
+    try {
+        const posts = await Post.find()
+        res.send(posts)
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+const createPost = async (req, res) => {
+  res.send("All posts fetched");
+};
 
 module.exports = {
     getAllPosts
