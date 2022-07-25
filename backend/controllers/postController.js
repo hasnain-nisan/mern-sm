2dd0e3e577a9ca7ad31d928704d798e11c37069a
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-07-25 17:22:02 +0600

    updatePost method added in controller backend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,32 +1,75 @@
-const { request } = require('express')
-const Post = require('../models/Post')
-
+const fs = require("fs");
+const { request } = require("express");
+const { mongoose } = require("mongoose");
+const { reset } = require("nodemon");
+const Post = require("../models/Post");
 
 const getAllPosts = async (req, res) => {
-    try {
-        const posts = await Post.find()
-        res.status(200).json(posts)
-    } catch (error) {
-        res.status(404).json({ message: error.message })
-    }
-}
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 const createPost = async (req, res) => {
-    let imageName = req.file.path
-    //adding imageName to req.body
-    req.body.file = imageName
+  let imageName = req.file.path;
+  //adding imageName to req.body
+  req.body.file = imageName;
 
-    const post = req.body
-    const newPost = new Post(post)
-    try {
-        await newPost.save()
-        res.status(201).json(newPost)
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+  const post = req.body;
+  const newPost = new Post(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const { title, message, creator, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    res.status(404).json({ message: `No post available with the id: ${_id}` });
+  }
+
+  const post = await Post.findOne({ _id });
+  post.title = title;
+  post.message = message;
+  post.creator = creator;
+  post.tags = tags;
+
+  if (req.file) {
+    let newFilePath = req.file.path;
+    let oldFilePath = post.file
+    //unlink old image
+    fs.unlinkSync(oldFilePath);
+    post.file = newFilePath;
+  }
+
+  try {
+    await post.save();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+
+  //   if(req.file){
+
+  //   } else {
+
+  //   }
+
+  res.json({
+    post: post,
+  });
 };
 
 module.exports = {
   getAllPosts,
   createPost,
+  updatePost,
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllPosts, createPost} = require("../controllers/postController");
+const { getAllPosts, createPost, updatePost} = require("../controllers/postController");
 
 const uploadMulterSingle = require('../utils/file-upload')
 const fileValidate = require('../utils/file-validation');
 
 router.get('/', getAllPosts)
-router.post("/", uploadMulterSingle, fileValidate, createPost);
+router.post("/", uploadMulterSingle, fileValidate, createPost)
+router.patch('/:id', uploadMulterSingle, updatePost)
 
 module.exports = router
